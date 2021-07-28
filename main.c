747f3a1948b844b2f900af449974a856eae6d40c
Vladimir Bakaev <bakaev.hmi@gmail.com>
2021-07-28 23:01:31 +1200

    tiding up

--- main.c
+++ main.c
@@ -21,45 +21,69 @@ static child_t childs[CHILDS_SIZE] = {
         {.mac = {1, 1, 1, 11, 12, 59}},
         {.mac = {1, 1, 1, 11, 12, 60}}
 };
-
-// called from mesh callback MESH_STARTED
+static uint8_t macs[10][6] = {
+    {1, 1, 1, 11, 12, 0}, // root - 0
+    {1, 1, 1, 11, 12, 1}, // 1
+    {1, 1, 1, 11, 12, 2}, // 2
+    {1, 1, 1, 11, 12, 3}, // 3
+    {1, 1, 1, 11, 12, 4}, // ..
+    {1, 1, 1, 11, 12, 5},
+    {1, 1, 1, 11, 12, 6},
+    {1, 1, 1, 11, 12, 7},
+    {1, 1, 1, 11, 12, 8},
+    {1, 1, 1, 11, 12, 9}, // 9
+};
+static uint8_t pairs[10][2] = {
+        {1,0},
+        {2,0},
+        {3,0}
+};
+// called from mesh callback MESH_EVENT_ROOT_ADDRESS
 void mesh_started() {
+    tree_set_root(macs[0], 0);
 }
 
-// only root
+// only called when event MESH_EVENT_CHILD_CONNECTED
 void mesh_child_connected(){
-    child_t root_child;
-    for(int i = 0; i < ROOT_CHILDS_SIZE; i++){
-        memcpy(root_child.mac, root_childs[i].mac, sizeof(uint8_t) * 6);
-        child_connected(&root_child, true);
+    child_t child;
+    for(int i = 1; i < 4; i++){
+        memcpy(child.mac, macs[i], sizeof(uint8_t) * 6);
+        tree_child_connected(&child, true);
     }
 }
 // from all other nodes
 void mesh_rx(){
+    for (int i = 4; i < 10; i++) {
+        ;
+    }
     int n = 6;
     fill_child(&childs[n],&root_childs[0]);
-    child_connected(&childs[n], false);
+    tree_child_connected(&childs[n], false);
     n = 3;
     fill_child(&childs[n],&root_childs[1]);
-    child_connected(&childs[n], false);
+    tree_child_connected(&childs[n], false);
     n = 0;
     fill_child(&childs[n],&childs[6]);
-    child_connected(&childs[n], false);
+    tree_child_connected(&childs[n], false);
     n = 1;
     fill_child(&childs[n],&childs[0]);
-    child_connected(&childs[n], false);
+    tree_child_connected(&childs[n], false);
 }
 
 void fill_child(child_t *child, child_t *parent) {
-    memcpy(child->parent_mac, parent->mac, mac_bytes);
+    memcpy(child->parent_mac, parent->mac, sizeof(uint8_t) * 6);
     child->level = parent->level + 1;
 }
 
 int main() {
+    // set root
     mesh_started();
+    // add childs to root
     mesh_child_connected();
+    // add childs to mediate nodes
     mesh_rx();
-    print_all_nodes();
+    // print
+    tree_print();
     return 0;
 }
 
